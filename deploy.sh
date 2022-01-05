a348438ecd7d2ab36045db0fa2550c8762eb430a
Christian Rodemeyer <atombrenner@gmail.com>
2022-01-05 14:26:03 +0100

    fix html redirect

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ export AWS_PROFILE="atombrenner"
 aws s3 cp build/static "${TARGET}/static" --recursive --cache-control "public,max-age=31536000" --no-progress
 aws s3 cp build s3://www.atombrenner.de/focus-crop-react --exclude "static/*" --recursive --cache-control "public,max-age=7200" --no-progress
 
-echo "<head><meta http-equiv=\"refresh\" content=\"0;URL='${TARGET/s3/https}'\"/></head>" >/tmp/redirect
+echo "<head><meta http-equiv=\"refresh\" content=\"0;URL='${TARGET/s3/https}/index.html'\"/></head>" >/tmp/redirect
 aws s3 cp /tmp/redirect "${TARGET}" --cache-control "public,max-age=31536000" --content-type "text/html" --no-progress
